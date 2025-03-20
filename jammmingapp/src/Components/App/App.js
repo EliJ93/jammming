9657638e83ed9feabf7be66df80072048b795ae2
Elijah Joseph <elijah.joseph93@gmail.com>
2025-03-20 11:42:46 -0700

    Fixed map function error

--- jammmingapp/src/Components/App/App.js
+++ jammmingapp/src/Components/App/App.js
@@ -1,32 +1,33 @@
-/*import logo from './logo.svg';
-import './App.css';
-import SearchBar from '../SearchBar';
-import Tracklist from '../Tracklist';
 
-function App() {
-  return (
-    
-    <><h1>Jammming!</h1>
-    <SearchBar />
-    <Tracklist /></>
-    
-  );
-}
-
-export default App;*/
 import React, { useState } from "react";
 import styles from "./App.module.css";
 import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 function App () {
   const [searchResults, setSearchResults] = useState([{
     name: "example track name 1",
     artist: "example track artist 1",
-    album: "example track album 1"
+    album: "example track album 1",
+    id: 1
   }, { 
     name: "example track name 2",
     artist: "example track artist 2",
-    album: "example track album 2"
+    album: "example track album 2",
+    id: 2
   } ]);
+  const [playlistName, setPlaylistName] = useState("Example Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([{
+    name: "example track name 1",
+    artist: "example track artist 1",
+    album: "example track album 1",
+    id: 1
+  }, { 
+    name: "example track name 2",
+    artist: "example track artist 2",
+    album: "example track album 2",
+    id: 2
+  }]);
+
     return (
         <div>
         <h1>
@@ -37,8 +38,9 @@ function App () {
           
           <div className={styles["App-playlist"]}>
             {/* <!-- Add a SearchResults component --> */}
-            <SearchResults userSearchResult={searchResults}/>
+            <SearchResults userSearchResults={searchResults}/>
             {/* <!-- Add a Playlist component --> */}
+            <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
           </div>
         </div>
       </div>
--- jammmingapp/src/Components/Tracklist/Tracklist.js
+++ jammmingapp/src/Components/Tracklist/Tracklist.js
@@ -1,12 +1,15 @@
 import React from "react";
+import styles from "./Tracklist.module.css";
+import Track from "../Track/Track";
 
-function Tracklist () {
+function Tracklist(props) {
     return (
-        <div className="TrackList">
+        <div className={styles.Tracklist}>
         {/* <!-- You will add a map method that renders a set of Track components  --> */}
-      <li>Track 1</li>
-      <li>Track 2</li>
-      <li>Track 3</li>
+
+      {props.userSearchResults.map((track) =>  
+          <Track track={track} key={track.id}/>
+)}
       </div>
     );
 }
